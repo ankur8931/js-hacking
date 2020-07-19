1ba08a3e30b7b997a90d88aea6f601a49e17c2f7
Ankur Chowdhary <achaud16@asu.edu>
2020-07-19 11:53:53 -0700

    Update system-command.js

--- system-command.js
+++ system-command.js
@@ -1,8 +1,5 @@
 <script>
-var spawn = require('child_process').spawn,
+var spawn = require('child_process').spawn;
 var ls  = spawn('ls', ['-l']);
-ls.stdout.on('data', function (data) {
-   console.log(data);
-   alert(data);
-});
+alert(ls);   
 </script>
